ecc618f1e2ebccee2a5d33f46c4bef09a179d9a7
Koplányi Dávid <55060571+KoplanyiDavid@users.noreply.github.com>
2024-09-23 10:43:33 +0200

    List Comprehension solution added for Exit answer to create missing_states list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import turtle, pandas as pd
 
-correct_guesses = 0
+guessed_states = []
 game_is_on = True
 input_screen_title = ""
 
@@ -14,16 +14,20 @@ data = pd.read_csv("./UsStatesQuiz/50_states.csv")
 states = data.state.to_list()
 
 while(game_is_on):
-    if correct_guesses > 0:
-        input_screen_title = f"{correct_guesses}/50 States Correct"
+    if len(guessed_states) > 0:
+        input_screen_title = f"{len(guessed_states)}/50 States Correct"
     else:
         input_screen_title = "Guess the State"
 
     answer_state = screen.textinput(title=input_screen_title, prompt="What's a state's name?").title()
     if answer_state == "Exit":
+        missing_states = [state for state in states if state not in guessed_states]
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("./UsStatesQuiz/states_to_learn.csv")
         break
+    
     if answer_state in states:
-        correct_guesses += 1
+        guessed_states.append(answer_state)
         state_data = data[data.state == answer_state]
         t = turtle.Turtle()
         t.hideturtle()
